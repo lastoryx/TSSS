89e14a0c024254f5f4ffd6cb9ff6cbd26a6071d9
Aaron Thompson <58874671+lastoryx@users.noreply.github.com>
2020-05-04 10:29:02 -0700

    Update.

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_thompsaa',
-  password        : 'Pumpkin2scream!',
-  database        : 'cs340_thompsaa'
+  user            : 'cs340_USERNAME',
+  password        : 'YOUR_PASSWORD_HERE',
+  database        : 'cs340_USERNAME'
 });
 
 module.exports.pool = pool;
